9bb9cb182091da633d21a8e21eedadc27a4e0421
emonc <emonc@DESKTOP-L392UFS>
2024-05-22 19:34:27 +0600

    genric Repo all method are Implement

---
+++ LawGuardPro.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawGuardPro.Infrastructure.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task AddAsync(T model);
+    IEnumerable<T> FindAllCaseAsync();
+    Task<bool> UpdateAsync(T model);
+    Task<bool> DeleteAsync(T model);
+}
---
+++ LawGuardPro.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using LawGuardPro.Infrastructure.Persistence.Context;
+
+namespace LawGuardPro.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly ApplicationDbContext _context;
+    
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddAsync(T model)
+    {
+        await _context.AddAsync(model);
+    }
+
+    public Task<bool> DeleteAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> FindAllCaseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+}
