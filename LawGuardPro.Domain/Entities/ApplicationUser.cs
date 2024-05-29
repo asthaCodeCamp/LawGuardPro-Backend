ae00ea97a126c5ca84c81232a13d91d6e285ac49
dotAsh <ashikshuvo45@gmail.com>
2024-05-29 01:10:30 -0600

    Merged with develop to solve conflict

--- LawGuardPro.Domain/Entities/ApplicationUser.cs
+++ LawGuardPro.Domain/Entities/ApplicationUser.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace LawGuardPro.Domain.Entities
-{
-    public class ApplicationUser
-    {
-    }
+namespace LawGuardPro.Domain.Entities;
+
+public class ApplicationUser : IdentityUser {
+     public string FirstName { get; set; }
+     public string LastName { get; set; }
+     public string PhoneNumber { get; set; }
+     public string CountryResidency { get; set; }
 }
