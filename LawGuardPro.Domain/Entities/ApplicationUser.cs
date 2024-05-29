4c7552293e1151d600e124e79c172b20674c886f
dotAsh <ashikshuvo45@gmail.com>
2024-05-28 23:48:36 -0600

    space aded after IdentityUser

--- LawGuardPro.Domain/Entities/ApplicationUser.cs
+++ LawGuardPro.Domain/Entities/ApplicationUser.cs
@@ -2,7 +2,7 @@
 
 namespace LawGuardPro.Domain.Entities;
 
-public class ApplicationUser : IdentityUser{
+public class ApplicationUser : IdentityUser {
      public string FirstName { get; set; }
      public string LastName { get; set; }
      public string PhoneNumber { get; set; }
