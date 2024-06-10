abf713658c086141949985a3d55b3e2d17f4683e
NafeesShihab12 <nafeesshihab121293@gmail.com>
2024-06-10 13:56:22 +0600

    change

--- LawGuardPro.API/Controllers/CaseController.cs
+++ LawGuardPro.API/Controllers/CaseController.cs
@@ -4,52 +4,51 @@ using System.Threading.Tasks;
 using MediatR;
 using LawGuardPro.Application.Features.Cases.Commands;
 
-namespace LawGuardPro.API.Controllers
+namespace LawGuardPro.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CaseController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public CaseController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public CaseController(IMediator mediator)
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCaseCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        if (result.IsSuccess())
         {
-            _mediator = mediator;
+            return Ok(result);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateCaseCommand command)
+        return BadRequest(result);
+    }
+
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetCasesByUserId(string userId, [FromQuery] int pageNumber, [FromQuery] int pageSize)
+    {
+        var query = new GetCasesByUserIdQuery(userId, pageNumber, pageSize);
+        var result = await _mediator.Send(query);
+
+        if (result.IsSuccess())
         {
-            var result = await _mediator.Send(command);
-
-            if (result.IsSuccess())
+            return Ok(new
             {
-                return Ok(result);
-            }
-
-            return BadRequest(result);
-        }
-
-        [HttpGet("user/{userId}")]
-        public async Task<IActionResult> GetCasesByUserId(string userId, [FromQuery] int pageNumber, [FromQuery] int pageSize)
-        {
-            var query = new GetCasesByUserIdQuery(userId, pageNumber, pageSize);
-            var result = await _mediator.Send(query);
-
-            if (result.IsSuccess())
-            {
-                return Ok(new
-                {
-                    Cases = result.Data.Cases,
-                    TotalCount = result.Data.TotalCount
-                });
-            }
-
-            return BadRequest(new
-            {
-                StatusCode = result.StatusCode,
-                Errors = result.Errors
+                Cases = result.Data.Cases,
+                TotalCount = result.Data.TotalCount
             });
         }
+
+        return BadRequest(new
+        {
+            StatusCode = result.StatusCode,
+            Errors = result.Errors
+        });
     }
 }
