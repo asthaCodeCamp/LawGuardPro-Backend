6eb5bd3e1da609113a77cc62c3a9c7ba1331890b
NafeesShihab12 <nafeesshihab121293@gmail.com>
2024-06-11 01:29:58 +0600

    random lawyer

--- LawGuardPro.Application/Features/Cases/Commands/CreateCaseCommandHandler.cs
+++ LawGuardPro.Application/Features/Cases/Commands/CreateCaseCommandHandler.cs
@@ -12,51 +12,55 @@ using System.Threading.Tasks;
 using LawGuardPro.Application.Common;
 using LawGuardPro.Application.Interfaces;
 
-namespace LawGuardPro.Application.Features.Cases.Commands
-{
-    public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Result<int>>
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IMapper _mapper;
+namespace LawGuardPro.Application.Features.Cases.Commands;
 
-        public CreateCaseCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Result<int>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateCaseCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<int>> Handle(CreateCaseCommand request, CancellationToken cancellationToken)
+    {
+        var caseEntity = _mapper.Map<Case>(request);
+
+        var maxCaseNumberString = await _unitOfWork.CaseRepository.GetMaxCaseNumberAsync();
+        int maxCaseNumber = 0;
+
+        if (!string.IsNullOrEmpty(maxCaseNumberString))
         {
-            _unitOfWork = unitOfWork;
-            _mapper = mapper;
+            maxCaseNumber = int.Parse(maxCaseNumberString);
         }
 
-        public async Task<Result<int>> Handle(CreateCaseCommand request, CancellationToken cancellationToken)
+        int nextCaseNumber = maxCaseNumber + 1;
+        caseEntity.CaseNumber = nextCaseNumber.ToString("D6");
+
+        caseEntity.Status = "working";
+        caseEntity.CreatedOn = DateTime.UtcNow;
+        caseEntity.LastUpdated = DateTime.UtcNow;
+        caseEntity.IsAttachmentAvailable = !string.IsNullOrEmpty(request.Attachment);
+        caseEntity.IsLawyerAssigned = false;
+
+        if (!string.IsNullOrEmpty(request.CaseType))
         {
-            var caseEntity = _mapper.Map<Case>(request);
-
-            var maxCaseNumberString = await _unitOfWork.CaseRepository.GetMaxCaseNumberAsync();
-            int maxCaseNumber = 0;
-
-            if (!string.IsNullOrEmpty(maxCaseNumberString))
+            var lawyers = await _unitOfWork.LawyerRepository.GetLawyersByTypeAsync(request.CaseType);
+            if (lawyers.Any())
             {
-                maxCaseNumber = int.Parse(maxCaseNumberString);
-            }
-
-            int nextCaseNumber = maxCaseNumber + 1;
-            caseEntity.CaseNumber = nextCaseNumber.ToString("D6");
-
-            caseEntity.Status = "working";
-            caseEntity.CreatedOn = DateTime.UtcNow;
-            caseEntity.LastUpdated = DateTime.UtcNow;
-            caseEntity.IsAttachmentAvailable = !string.IsNullOrEmpty(request.Attachment);
-            caseEntity.IsLawyerAssigned = false;
-
-            var lawyer = await _unitOfWork.LawyerRepository.GetFirstAsync(l => l.LawyerType == request.CaseType);
-            if (lawyer != null)
-            {
+                var random = new Random();
+                var lawyer = lawyers.OrderBy(x => random.Next()).First();
                 caseEntity.LawyerId = lawyer.LawyerId;
                 caseEntity.IsLawyerAssigned = true;
             }
-
-            await _unitOfWork.CaseRepository.AddAsync(caseEntity);
-            await _unitOfWork.CommitAsync();
-
-            return Result<int>.Success(caseEntity.CaseId);
         }
+
+        await _unitOfWork.CaseRepository.AddAsync(caseEntity);
+        await _unitOfWork.CommitAsync();
+
+        return Result<int>.Success(caseEntity.CaseId);
     }
 }
--- LawGuardPro.Infrastructure/Repositories/LawyerRepository.cs
+++ LawGuardPro.Infrastructure/Repositories/LawyerRepository.cs
@@ -2,32 +2,26 @@
 using LawGuardPro.Domain.Entities;
 using LawGuardPro.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LawGuardPro.Infrastructure.Repositories
+namespace LawGuardPro.Infrastructure.Repositories;
+
+public class LawyerRepository : Repository<Lawyer>, ILawyerRepository
 {
-    public class LawyerRepository : Repository<Lawyer>, ILawyerRepository
+    public LawyerRepository(ApplicationDbContext context) : base(context)
     {
-        public LawyerRepository(ApplicationDbContext context) : base(context)
-        {
-        }
-
-        public async Task<IEnumerable<Lawyer>> GetLawyersByTypeAsync(string lawyerType)
-        {
-            return await _context.Lawyers
-                                 .Where(l => l.LawyerType == lawyerType)
-                                 .ToListAsync();
-        }
-
-        public async Task<Lawyer> GetLawyerByIdAsync(int lawyerId)
-        {
-            return await _context.Lawyers
-                                 .FirstOrDefaultAsync(l => l.LawyerId == lawyerId);
-        }
     }
-}
 
+    public async Task<IEnumerable<Lawyer>> GetLawyersByTypeAsync(string lawyerType)
+    {
+        return await _context.Lawyers
+                             .Where(l => l.LawyerType == lawyerType)
+                             .ToListAsync();
+    }
+
+    public async Task<Lawyer> GetLawyerByIdAsync(int lawyerId)
+    {
+        return await _context.Lawyers
+                             .FirstOrDefaultAsync(l => l.LawyerId == lawyerId);
+    }
+
+}
