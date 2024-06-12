f1c47c64bf543174f5cbbc8764f2ed64c7bc7bef
NafeesShihab12 <nafeesshihab121293@gmail.com>
2024-06-12 12:42:08 +0600

    add lawyer

--- LawGuardPro.Application/Features/Cases/Commands/CreateCaseCommandHandler.cs
+++ LawGuardPro.Application/Features/Cases/Commands/CreateCaseCommandHandler.cs
@@ -7,7 +7,7 @@ using LawGuardPro.Domain.Common.Enums;
 
 namespace LawGuardPro.Application.Features.Cases.Commands;
 
-public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Result<int>>
+public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, IResult<int>>
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
@@ -16,45 +16,6 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Resul
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
-    }
-
-    public async Task<Result<int>> Handle(CreateCaseCommand request, CancellationToken cancellationToken)
-    {
-        var caseEntity = _mapper.Map<Case>(request);
-
-        var maxCaseNumberString = await _unitOfWork.CaseRepository.GetMaxCaseNumberAsync();
-        int maxCaseNumber = 0;
-
-        if (!string.IsNullOrEmpty(maxCaseNumberString))
-        {
-            maxCaseNumber = int.Parse(maxCaseNumberString);
-        }
-
-        int nextCaseNumber = maxCaseNumber + 1;
-        caseEntity.CaseNumber = nextCaseNumber.ToString("D6");
-
-        caseEntity.Status = "working";
-        caseEntity.CreatedOn = DateTime.UtcNow;
-        caseEntity.LastUpdated = DateTime.UtcNow;
-        caseEntity.IsAttachmentAvailable = !string.IsNullOrEmpty(request.Attachment);
-        caseEntity.IsLawyerAssigned = false;
-
-        if (!string.IsNullOrEmpty(request.CaseType))
-        {
-            var lawyers = await _unitOfWork.LawyerRepository.GetLawyersByTypeAsync(request.CaseType);
-            if (lawyers.Any())
-            {
-                var random = new Random();
-                var lawyer = lawyers.OrderBy(x => random.Next()).First();
-                caseEntity.LawyerId = lawyer.LawyerId;
-                caseEntity.IsLawyerAssigned = true;
-            }
-        }
-
-        await _unitOfWork.CaseRepository.AddAsync(caseEntity);
-        await _unitOfWork.CommitAsync();
-
-        return Result<int>.Success(caseEntity.CaseId);
     }
 
     public async Task<IResult<int>> Handle(CreateCaseCommand request, CancellationToken cancellationToken)
@@ -68,17 +29,23 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Resul
         {
             maxCaseNumber = int.Parse(maxCaseNumberString);
         }
-        int nextCaseNumber = maxCaseNumber + 1;
 
+        int nextCaseNumber = maxCaseNumber + 1;
         caseEntity.CaseNumber = nextCaseNumber.ToString("D6");
+
         caseEntity.Status = CaseStatus.Working;
         caseEntity.CreatedOn = DateTime.UtcNow;
         caseEntity.LastUpdated = DateTime.UtcNow;
 
-        var lawyer = await _unitOfWork.LawyerRepository.GetFirstAsync(l => l.LawyerType == request.CaseType);
-        if (lawyer != null)
+        if (!string.IsNullOrEmpty(request.CaseType))
         {
-            caseEntity.LawyerId = lawyer.LawyerId;
+            var lawyers = await _unitOfWork.LawyerRepository.GetLawyersByTypeAsync(request.CaseType);
+            if (lawyers.Any())
+            {
+                var random = new Random();
+                var lawyer = lawyers.OrderBy(x => random.Next()).First();
+                caseEntity.LawyerId = lawyer.LawyerId;
+            }
         }
 
         await _unitOfWork.CaseRepository.AddAsync(caseEntity);
@@ -86,4 +53,5 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, Resul
 
         return Result<int>.Success(caseEntity.CaseId);
     }
+
 }
