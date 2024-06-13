ad2fdc949b8eb22db1c5d6504240ebecfb599ec8
NafeesShihab12 <nafeesshihab121293@gmail.com>
2024-06-13 18:08:31 +0600

    Migration added

---
+++ LawGuardPro.Infrastructure/Migrations/20240613120802_CreateTableOfCasesLawyersEmailsAddressesAdded.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LawGuardPro.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableOfCasesLawyersEmailsAddressesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresss",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AddressType = table.Column<int>(type: "integer", nullable: false),
+                    BillingName = table.Column<string>(type: "text", nullable: true),
+                    AddressLine1 = table.Column<string>(type: "text", nullable: true),
+                    AddressLine2 = table.Column<string>(type: "text", nullable: true),
+                    Town = table.Column<string>(type: "text", nullable: true),
+                    PostalCode = table.Column<int>(type: "integer", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresss", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresss_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FromName = table.Column<string>(type: "text", nullable: false),
+                    FromEmail = table.Column<string>(type: "text", nullable: false),
+                    ToName = table.Column<string>(type: "text", nullable: false),
+                    ToEmail = table.Column<string>(type: "text", nullable: false),
+                    IsSent = table.Column<bool>(type: "boolean", nullable: false),
+                    Subject = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lawyers",
+                columns: table => new
+                {
+                    LawyerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LawyerName = table.Column<string>(type: "text", nullable: true),
+                    LawyerType = table.Column<string>(type: "text", nullable: true),
+                    Rating = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lawyers", x => x.LawyerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    CaseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CaseNumber = table.Column<string>(type: "text", nullable: true),
+                    CaseName = table.Column<string>(type: "text", nullable: true),
+                    CaseType = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LawyerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.CaseId);
+                    table.ForeignKey(
+                        name: "FK_Cases_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cases_Lawyers_LawyerId",
+                        column: x => x.LawyerId,
+                        principalTable: "Lawyers",
+                        principalColumn: "LawyerId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresss_UserId",
+                table: "Addresss",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cases_LawyerId",
+                table: "Cases",
+                column: "LawyerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cases_UserId",
+                table: "Cases",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresss");
+
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "Lawyers");
+        }
+    }
+}
