e9908f776e819e9018d1349acd8d28e5cf5ed525
NafeesShihab12 <nafeesshihab121293@gmail.com>
2024-06-23 15:04:41 +0600

    change to local connection string

--- LawGuardPro.Infrastructure/DependencyInjection.cs
+++ LawGuardPro.Infrastructure/DependencyInjection.cs
@@ -24,7 +24,7 @@ public static class DependencyInjection
         IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-              options.UseNpgsql(configuration.GetConnectionString("ProdSQLConnection")));
+              options.UseNpgsql(configuration.GetConnectionString("DefaultSQLConnection")));
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddScoped<IAddressRepository, AddressRepository>();
 
